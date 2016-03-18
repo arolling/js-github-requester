9a1abaf1089467c5cf01663973779c37b147c6a3
Abigail Rolling <arolling@gmail.com>
2016-03-18 09:53:01 -0700

    Style repo display

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,6 +1,12 @@
 var getRepos = require("./../js/getRepos.js").getRepos;
 
 $(document).ready(function(){
-  getRepos();
+  $('#enterName').submit(function(e) {
+    e.preventDefault();
+    var userName = $('#gitName').val();
+    $('#gitName').val('');
+    getRepos(userName);
+  });
+
 
 });
--- js/getRepos.js
+++ js/getRepos.js
@@ -1,19 +1,26 @@
 var apiKey = require("./../.env").apiKey;
 
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+exports.getRepos = function(userName){
+  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
     console.log(response);
-    $("#userName").text(response.name);
-    $("#userInfo").append("<a href='" + response.html_url + "' target='_blank'>" + response.login + "</a><br>");
-    $("#userInfo").append("<a href='" + response.blog + "' target='_blank'>Blog</a>");
+
+    $("#userInfo").empty();
+    $("#userName").html("User Name: <a href='" + response.html_url + "' target='_blank'>" + response.login + "</a><br>");
+    $("#userName").append(response.name);
+    if(response.blog !== null) {
+      $("#userInfo").append("<a href='" + response.blog + "' target='_blank'>Blog</a>");
+    }
+    $(".hideme").show();
+
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 
-  $.get('https://api.github.com/users/daneden/repos?access_token=' + apiKey ).then(function(response){
+  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey ).then(function(response){
     console.log(response);
+    $('#userRepos').empty();
     response.forEach(function(repo){
-      $('#userRepos').append('<a href="' + repo.html_url + '" class="list-group-item"  target="_blank">' + repo.name + ' - ' + repo.description + '</a>');
+      $('#userRepos').append('<a href="' + repo.html_url + '" class="list-group-item"  target="_blank"><h4 class="list-group-item-heading">' + repo.name + '</h4><p class="list-group-item-text">' + repo.description + '</p></a>');
     });
   }).fail(function(error){
     console.log(error.responseJSON.message);
